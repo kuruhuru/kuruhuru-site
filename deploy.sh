c682f27b02cb612a92abaf4a26fc63c9eb32c0ea
kuruhuru <kuruhuru@gmail.com>
2020-01-28 21:18:59 +0700

    Added Publications

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Clear Public folder
+cd public
+git checkout master
+git pull
+git rm -r .
+cd ..
+
 # Build the project. if using a theme, replace with `hugo -t <YOURTHEME>`
-hugo -b https://kuruhuru.github.io/
+./hugo -b https://kuruhuru.github.io -t cupper-hugo-theme
 
 # Go To Public folder
 cd public
@@ -9,7 +16,7 @@ cd public
 git add .
 
 # Commit changes.
-git commit -m "Rebuilding site"
+git commit -m "Update site"
 
 # Push source and build repos.
 git push origin master
