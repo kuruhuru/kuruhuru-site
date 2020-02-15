429c745dd1e4d22994b2cad651dd862fc3308cbd
Peter Matyukov <kuruhuru@gmail.com>
2020-02-15 19:21:18 +0700

    Added так долго, added deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,14 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+rem Clear Public folder
+cd public
+git checkout master
+git pull
+git rm -r .
+cd ..
+
 rem Build the project. if using a theme, replace with `hugo -t <YOURTHEME>`
-hugo -b https://kuruhuru.github.io/
+hugo -b https://kuruhuru.github.io -t cupper-hugo-theme
 
 rem Go To Public folder
 cd public
@@ -9,7 +16,7 @@ rem Add changes to git.
 git add .
 
 rem Commit changes.
-git commit -m "Rebuilding site"
+git commit -m "Update site"
 
 rem Push source and build repos.
 git push origin master
